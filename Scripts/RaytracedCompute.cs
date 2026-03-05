2cdfaa264f0fbbaacb830e507185be0ad364e2a5
DrBellubins <88703839+DrBellubins@users.noreply.github.com>
2026-03-04 20:36:45 -0500

    adasfasg

--- Scripts/RaytracedCompute.cs
+++ Scripts/RaytracedCompute.cs
@@ -4,25 +4,23 @@ using System.Runtime.InteropServices;
 
 public partial class RaytracedCompute : Node
 {
-    [Export] public Resource ShaderSource;
-    
-    [Export]
-    public NodePath OutputTextureRectPath { get; set; }
+    [Export] public RDShaderFile ShaderSource;
 
-    [Export]
-    public Vector2I Resolution { get; set; } = new Vector2I(960, 540);
+    [Export] public NodePath OutputTextureRectPath { get; set; }
+
+    [Export] public Vector2I Resolution { get; set; } = new Vector2I(960, 540);
 
     private RenderingDevice _rd;
     private Rid _shaderRid;
     private Rid _pipelineRid;
 
     private Rid _outputTexRid;
-    private Rid _outputTexViewRid;
 
     private Rid _paramsBufferRid;
     private Rid _uniformSetRid;
 
-    private Texture2D _displayTexture;
+    private TextureRect _output;
+    private ImageTexture _imageTexture;
 
     [StructLayout(LayoutKind.Sequential)]
     private struct Params
@@ -39,12 +37,13 @@ public partial class RaytracedCompute : Node
 
     public override void _Ready()
     {
+        _output = GetNode<TextureRect>(OutputTextureRectPath);
+
         _rd = RenderingServer.GetRenderingDevice();
 
         CreateOutputTexture();
         CreateComputePipeline();
         CreateUniforms();
-
         SetupDisplay();
     }
 
@@ -58,7 +57,6 @@ public partial class RaytracedCompute : Node
         FreeRid(ref _uniformSetRid);
         FreeRid(ref _paramsBufferRid);
 
-        FreeRid(ref _outputTexViewRid);
         FreeRid(ref _outputTexRid);
 
         FreeRid(ref _pipelineRid);
@@ -73,11 +71,14 @@ public partial class RaytracedCompute : Node
         }
 
         DispatchCompute((float)delta);
+
+        // Minimal display path: GPU -> CPU -> ImageTexture (slow but simple)
+        UpdateDisplayFromGpu();
     }
 
     private void CreateOutputTexture()
     {
-        var tf = new RDTextureFormat();
+        RDTextureFormat tf = new RDTextureFormat();
         tf.Width = (uint)Resolution.X;
         tf.Height = (uint)Resolution.Y;
         tf.Depth = 1;
@@ -87,22 +88,25 @@ public partial class RaytracedCompute : Node
         tf.Format = RenderingDevice.DataFormat.R16G16B16A16Sfloat;
         tf.UsageBits =
             RenderingDevice.TextureUsageBits.StorageBit |
-            RenderingDevice.TextureUsageBits.CanUpdateBit |
-            RenderingDevice.TextureUsageBits.SamplingBit |
             RenderingDevice.TextureUsageBits.CanCopyFromBit;
 
-        var tv = new RDTextureView();
+        RDTextureView tv = new RDTextureView();
 
-        _outputTexRid = _rd.TextureCreate(tf, tv, Array.Empty<byte[]>());
-        _outputTexViewRid = _rd.TextureCreateSharedView(_outputTexRid, tv);
+        // Godot C# expects Godot.Collections.Array<byte[]>
+        Godot.Collections.Array<byte[]> initialData = new Godot.Collections.Array<byte[]>();
+
+        _outputTexRid = _rd.TextureCreate(tf, tv, initialData);
     }
 
     private void CreateComputePipeline()
     {
-        // You must create an RDShaderFile resource in the editor that points to raytrace_sdf.compute.glsl
-        // and assign it to this script or load it here. Keeping it minimal: load from res://
-        var shaderFile = GD.Load<RDShaderFile>(ShaderSource.ResourcePath);
-        var spirv = shaderFile.GetSpirV();
+        if (ShaderSource == null)
+        {
+            GD.PushError("ShaderSource is null. Assign the RDShaderFile that imports Shaders/Raytracing/raytracer.glsl.");
+            return;
+        }
+
+        RDShaderSpirV spirv = ShaderSource.GetSpirV();
 
         _shaderRid = _rd.ShaderCreateFromSpirV(spirv);
         _pipelineRid = _rd.ComputePipelineCreate(_shaderRid);
@@ -110,26 +114,25 @@ public partial class RaytracedCompute : Node
 
     private void CreateUniforms()
     {
-        var p = new Params();
+        Params p = new Params();
         p.Resolution = new Vector2(Resolution.X, Resolution.Y);
         p.Time = 0.0f;
 
-        p.CamPos = new Vector3(0.0f, 0.0f, 0.0f);
+        p.CamPos = new Vector3(0.0f, 0.0f, -3.0f);
         p.CamForward = new Vector3(0.0f, 0.0f, 1.0f);
         p.CamRight = new Vector3(1.0f, 0.0f, 0.0f);
         p.CamUp = new Vector3(0.0f, 1.0f, 0.0f);
         p.FovY = Mathf.DegToRad(60.0f);
 
         byte[] bytes = StructToBytes(p);
-
         _paramsBufferRid = _rd.StorageBufferCreate((uint)bytes.Length, bytes);
 
-        var u0 = new RDUniform();
+        RDUniform u0 = new RDUniform();
         u0.UniformType = RenderingDevice.UniformType.Image;
         u0.Binding = 0;
-        u0.AddId(_outputTexViewRid);
+        u0.AddId(_outputTexRid);
 
-        var u1 = new RDUniform();
+        RDUniform u1 = new RDUniform();
         u1.UniformType = RenderingDevice.UniformType.StorageBuffer;
         u1.Binding = 1;
         u1.AddId(_paramsBufferRid);
@@ -143,7 +146,6 @@ public partial class RaytracedCompute : Node
         p.Resolution = new Vector2(Resolution.X, Resolution.Y);
         p.Time = (float)Time.GetTicksMsec() * 0.001f;
 
-        // Very simple camera (static). If you want: pull from a Camera3D node.
         p.CamPos = new Vector3(0.0f, 0.0f, -3.0f);
         p.CamForward = new Vector3(0.0f, 0.0f, 1.0f);
         p.CamRight = new Vector3(1.0f, 0.0f, 0.0f);
@@ -164,21 +166,36 @@ public partial class RaytracedCompute : Node
         _rd.ComputeListEnd();
 
         _rd.Submit();
+        _rd.Sync();
     }
 
     private void SetupDisplay()
     {
-        var tr = GetNode<TextureRect>(OutputTextureRectPath);
+        Image img = Image.Create(Resolution.X, Resolution.Y, false, Image.Format.Rgba8);
+        _imageTexture = ImageTexture.CreateFromImage(img);
 
-        // Create a Godot Texture2D wrapper around the RID.
-        // In Godot 4, you can use Texture2DRD.
-        var tex = new Texture2DRD();
-        tex.TextureRid = _outputTexRid;
+        _output.Texture = _imageTexture;
+        _output.ExpandMode = TextureRect.ExpandModeEnum.IgnoreSize;
+        _output.StretchMode = TextureRect.StretchModeEnum.Scale;
+    }
 
-        _displayTexture = tex;
-        tr.Texture = _displayTexture;
-        tr.ExpandMode = TextureRect.ExpandModeEnum.IgnoreSize;
-        tr.StretchMode = TextureRect.StretchModeEnum.Scale;
+    private void UpdateDisplayFromGpu()
+    {
+        // Readback from GPU. Then convert to RGBA8 for UI.
+        // Godot returns an Image in the texture's format.
+        Image img = _rd.TextureGetData(_outputTexRid, 0);
+
+        if (img == null)
+        {
+            return;
+        }
+
+        if (img.GetFormat() != Image.Format.Rgba8)
+        {
+            img.Convert(Image.Format.Rgba8);
+        }
+
+        _imageTexture.Update(img);
     }
 
     private static byte[] StructToBytes<T>(T data) where T : struct
