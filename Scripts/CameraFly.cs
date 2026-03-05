b925bc4d14fc7d9265a1d23cd8f6578845b23701
DrBellubins <88703839+DrBellubins@users.noreply.github.com>
2026-03-04 21:38:26 -0500

    camera work

--- Scripts/CameraFly.cs
+++ Scripts/CameraFly.cs
@@ -14,8 +14,10 @@ public partial class CameraFly : Camera3D
     {
         var forwardBackInput = Input.GetAxis("backward", "forward");
         var leftRightInput = Input.GetAxis("left", "right");
+        var upDownInput = Input.GetAxis("down", "up");
 
         Translate(new Vector3(0f, 0f, forwardBackInput * Speed * (float)delta));
         Translate(new Vector3(leftRightInput * Speed * (float)delta, 0f, 0f));
+        Translate(new Vector3(0f, upDownInput * Speed * (float)delta, 0f));
     }
 }
